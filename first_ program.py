10409e01e2e30a90d95c4e9308598fa1c5c652d4
Ibrahim Usman <bravegalus@gmail.com>
2026-09-16 12:05:09 +0400

    First commit

---
+++ first_ program.py
@@ -0,0 +1,26 @@
+name = "Ibrahim Usman" 
+age = 26
+print("My name is", name, "and I am", age, "years old.")
+city = "Dubai"
+height = 5.6
+print("I live in", city, "and my height is", height, "feet.")
+print("I am a physical & cctv operator transitioning into the tech field and this is my first attempt at coding.")
+print(type(name))
+print(type(age))
+print(type(city))
+print(type(height))
+birth_year = 2026 - age
+print("Approximate birth year:", birth_year)
+a = 10
+b = 5
+print("addition:", a + b)
+print("subtraction:", a - b)
+print("multiplication:", a * b)
+print("division:", a / b)
+favorite_food = input("What is your favorite food? ")
+print("Your favorite food is:", favorite_food)
+favorite_color = input("What is your favorite color? ")
+print("Your favorite color is:", favorite_color)
+print("FOOD =", favorite_food, "COLOR =", favorite_color)
+age_input = int(input("Please enter your age: "))
+print(type(age_input))
