011181d806166f56790d2455641077732aa95496
Ibrahim Usman <bravegalus@gmail.com>
2026-09-16 20:18:25 +0400

    add python practice program

---
+++ practice.py
@@ -0,0 +1,15 @@
+name = input("What is your name: ")
+goal = input("What are you learning? ")
+
+print("Hello",name)
+print("your current goal is:",goal)
+print("keep learning and practicing!")      
+age=int(input("How old are you: "))
+
+future_age=age+5
+
+print("In 5 years you will be",future_age,"years old")
+if age >= 18:
+    print("You are an adult.")  
+else:
+    print("You are under 18.")
